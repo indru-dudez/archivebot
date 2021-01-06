08610076123929949738b7355ed67f68b155281e
indraabhushan.pr <68438304+indru-dudez@users.noreply.github.com>
2021-01-06 00:49:44 -0800

    Update config.py

--- archivebot/config.py
+++ archivebot/config.py
@@ -6,8 +6,8 @@ import toml
 default_config = {
     "telegram": {
         "userbot": True,
-        "api_key": "1484527342:AAEktw6k1lB4yyMpzuXAxtGRIiPrvhXBUGk",
-        "phone_number": "your_phone_number (empty if not in userbot mode)",
+        "api_key": "1425278481:AAHTCRS9agrLu4-TX5w-ax_IZ7NTkFz7L7s",
+        "phone_number": "",
         "app_api_id": 1555704,
         "app_api_hash": "f08030f122370b15dbeaefb39cb0f693",
     },
