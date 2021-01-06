e30291ebbc32fac336d2935ebb4bfc93e4f4fd9f
indraabhushan.pr <68438304+indru-dudez@users.noreply.github.com>
2021-01-06 01:38:29 -0800

    Update config.py

--- archivebot/config.py
+++ archivebot/config.py
@@ -19,3 +19,23 @@ default_config = {
     },
     "zip": {"volume_size": "1400m",},
 }
+
+config_path = os.path.expanduser("~/.archivebot/config.py")
+
+if not os.path.exists(config_path):
+    with open(config_path, "w") as file_descriptor:
+        toml.dump(default_config, file_descriptor)
+    print("Please adjust the configuration file at '~/.config/archivebot.toml'")
+    sys.exit(1)
+else:
+    config = toml.load(config_path)
+
+    # Set default values for any missing keys in the loaded config
+    for key, category in default_config.items():
+        if key not in config:
+            config[key] = category
+            continue
+
+        for option, value in category.items():
+            if option not in config[key]:
+                config[key][option] = value
